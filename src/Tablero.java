02450d8a849ed5b4d4cf5ff9e590aba399d70d39
Alejandro Casiva <alec667@gmail.com>
2024-11-03 00:32:59 -0300

    crea y pinta el tablero de ajedrez

--- src/Tablero.java
+++ src/Tablero.java
@@ -4,17 +4,44 @@ public class Tablero {
 
     //constructor:
     public Tablero(int size) {
+        tablero = new String[size][size];
     }
 
     public void pintarTablero() {
+        for (int i = 0; i < getTablero().length; i++) {
+            for (int j = 0; j < getTablero().length; j++) {
+                if (j <= i) {
+                    getTablero()[i][j] = "[Rojo]";
+
+                } else {
+                    getTablero()[i][j] = "[Azul]";
+                }
+
+            }
+        }
     }
 
 
     //dibuja el tablero en terminal:
     public void printTablero() {
+        for (int i = 0; i < getTablero().length; i++) {
+            for (int j = 0; j < getTablero().length; j++) {
+                System.out.print(getTablero()[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     public int nroDeCeldasRojas() {
         return 0;
+    }
+
+    //Getters & setters:
+    public String[][] getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(String[][] tablero) {
+        this.tablero = tablero;
     }
 }
