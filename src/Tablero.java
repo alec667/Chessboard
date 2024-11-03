52aded9b97c6e2a19f67c29138a31a87620c83f5
Alejandro Casiva <alec667@gmail.com>
2024-11-03 00:31:06 -0300

    agrega clase Tablero.java

---
+++ src/Tablero.java
@@ -0,0 +1,20 @@
+public class Tablero {
+
+    private String[][] tablero;
+
+    //constructor:
+    public Tablero(int size) {
+    }
+
+    public void pintarTablero() {
+    }
+
+
+    //dibuja el tablero en terminal:
+    public void printTablero() {
+    }
+
+    public int nroDeCeldasRojas() {
+        return 0;
+    }
+}
