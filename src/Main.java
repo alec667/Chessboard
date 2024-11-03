f1dd7f653e5c334ae25f27e200a3e152a3878ff5
Alejandro Casiva <alec667@gmail.com>
2024-11-03 00:16:13 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * Considera a un muchacho que se deleita pintando su tablero de ajedrez. Su objetivo es saturar cada celda con
+ * tonos rojos o azules.
+ * En busca de darle una identidad propia, procura tener un equilibrio entre los espacios rojos y azules,
+ * previniendo que dos filas o columnas contengan el mismo número de celdas rojas. ¿Lograría pintar el tablero de
+ * ajedrez siguiendo estas pautas?
+ * ¿Qué acontecería si, en vez de un tablero de ajedrez común de 8x8, tuviera uno monumental de 1000x1000?
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
